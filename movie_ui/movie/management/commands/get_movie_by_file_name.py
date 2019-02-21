20c7021cae7f3fd5e22da5f169dd5824b4a98127
En Zhou <zhouen.nathan@gmail.com>
2019-02-21 12:36:36 +0200

    Updated travis and typos

--- movie_ui/movie/management/commands/get_movie_by_file_name.py
+++ movie_ui/movie/management/commands/get_movie_by_file_name.py
@@ -1,11 +1,9 @@
-import argparse
-
 from django.core.management.base import BaseCommand
 from movie.utils.movie_crawler import MovieCrawler
 
 
 class Command(BaseCommand):
-    help = 'This a command crawsl movie info from IMDB with the given movie filename.'
+    help = 'This a command crawls movie info from IMDB with the given movie filename.'
 
     def add_arguments(self, parser):
         parser.add_argument(
@@ -17,5 +15,4 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         crawler = MovieCrawler(kwargs['file'])
-        # import ipdb; ipdb.set_trace()
         crawler.crawl_movie()
